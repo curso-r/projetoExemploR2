47c952df92e7b2f1fc4747d132967de2fb202db0
Fernando Corrêa <fptcorrea@gmail.com>
2022-07-14 21:21:38 -0300

    deixando a base de ajuste do modelo em ingles

--- R/modelo.R
+++ R/modelo.R
@@ -1,7 +1,7 @@
 source("R/pacotes.R")
 
-mtcars_modelo <- readRDS("data/mtcars_modelo.rds")
+mtcars_model <- readRDS("data/mtcars_modelo.rds")
 # lendo dados trabalhado
 
-modelo <- lm(mpg ~ coluna_regressao, data = mtcars_modelo)
+modelo <- lm(mpg ~ coluna_regressao, data = mtcars_model)
 # ajusta modelo linear
