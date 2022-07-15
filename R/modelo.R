b896d146635f8c6fe27f449ce151ee14b4641e04
Fernando Corrêa <fptcorrea@gmail.com>
2022-07-14 21:11:40 -0300

    arrumando typo do arquivo de modelo

--- R/modelo.R
+++ R/modelo.R
@@ -1,6 +1,6 @@
 source("R/pacotes.R")
 
 mtcars_modelo <- readRDS("data/mtcars_modelo.rds")
-# lendo dados trabalhos
+# lendo dados trabalhado
 
 modelo <- lm(mpg ~ coluna_regressao, data = mtcars_modelo)
