18500934e0fba2d8f4bf91ede88c744a3d9f0b9a
Fernando Corrêa <fptcorrea@gmail.com>
2022-07-14 21:21:58 -0300

    comentando funcoes

--- R/categoriza_wt.R
+++ R/categoriza_wt.R
@@ -1,7 +1,9 @@
+# cria categorias pesado e leve
 categoriza_wt <- function(coluna, ponto_de_corte){
   if_else(coluna > ponto_de_corte, "Pesado", "Leve")
 }
 
+# cria categorias alto e baixo
 categoriza <- function(coluna, ponto_de_corte){
   if_else(coluna > ponto_de_corte, "Alto", "Baixo")
 }
